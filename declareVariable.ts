84c57aeaf1adf1238f30366438d0e24fc7a2f325
Varun Pandey <65996450+varundwt@users.noreply.github.com>
2024-08-05 18:27:20 +0530

    "declaring variables with ts"

---
+++ declareVariable.ts
@@ -0,0 +1,11 @@
+//Syntax for declaring variables with TS.
+//let variableName: type = value;
+
+let greeting: string = 'Hello Varun';
+console.log(greeting);
+
+//After this we won't be able to assign any diffrent data type to greeting variable.
+//So there is more safety with with TS where JS simply allows almost anything.
+
+//If we type ' greeting. ' then it will show uys the list of methods associated with strings since greeting is a string type.
+//Same would happen to diffent data tyoes and their methods.
